58823583f70b8b57607fbe8ec54ccd386223264d
techbot <techbot121@gmail.com>
2017-06-09 00:33:09 +0200

    actual fix

--- lua/discordrelay/sv_discordrelay.lua
+++ lua/discordrelay/sv_discordrelay.lua
@@ -262,7 +262,7 @@ function discordrelay.InitializeModules()
     end
 end
 
-hook.Add("NotagainPostLoad", "DiscordRelayLoadModules", InitializeModules)
+hook.Add("NotagainPostLoad", "DiscordRelayLoadModules", discordrelay.InitializeModules)
 
 function discordrelay.reload()
     DiscordrelayInit()
