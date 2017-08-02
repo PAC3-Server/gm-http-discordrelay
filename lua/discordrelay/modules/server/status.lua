6c21b5f09206ff3de347a0304fd987d9211df37b
R.Réno <reno.reboursier@gmail.com>
2017-08-02 17:36:27 +0200

    ???

--- lua/discordrelay/modules/server/status.lua
+++ lua/discordrelay/modules/server/status.lua
@@ -4,10 +4,8 @@ local discordrelay = discordrelay
 function status.Handle(input,previous,future)
     if input.author.bot ~= true and discordrelay.util.startsWith("status", input.content) then
         local embeds = {}
-        local players = player.GetAll()
         local cache = discordrelay.AvatarCache -- todo check if not nil
-        for i=1,#players do
-            local ply = players[i]
+        for i,ply in pairs(player.GetAll()) do
             local commid = util.SteamIDTo64(ply:SteamID()) -- move to player meta?
             embeds[i] = {
                 ["author"] = {
@@ -17,7 +15,7 @@ function status.Handle(input,previous,future)
                 ["color"] = (ply:IsAFK() ~= nil and ply:IsAFK()) and 0xffff00 or 0x00b300
             }
         end
-        if #players > 0 then
+        if player.GetCount() > 0 then
             discordrelay.ExecuteWebhook(discordrelay.webhookid, discordrelay.webhooktoken, {
                 ["username"] = "Server status:",
                 ["avatar_url"] = discordrelay.avatar,
