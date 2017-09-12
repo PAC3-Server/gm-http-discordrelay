ad1bbf47765a6609c82be586b7ef7302f3cd90e7
techbot <techbot121@gmail.com>
2017-09-12 15:51:27 +0200

    but it's required here

---
+++ lua/discordrelay/modules/server/luaerror_to_channel.lua
@@ -0,0 +1,52 @@
+local scriptlog = {}
+local discordrelay = discordrelay
+
+function scriptlog.Init()
+    local easylua = requirex('easylua')
+    local luadev = requirex('luadev')
+    local logChannel = "337186861111836684"
+    if not easylua or not luadev then discordrelay.log(2,"easylua or luadev not found, scriptlogging disabled.") return false end
+
+    local blacklist = {"suicided", "Bad SetLocalOrigin","Changing collision rules"}
+    local logBuffer = ""
+    local abort = 0
+
+    timer.Create("DiscordRelayAddLog", 5, 0, function()
+        if abort >= 5 then discordrelay.log(3,"DiscordRelayAddLog failed DESTROYING") timer.Destroy("DiscordRelayAddLog") return end -- prevent spam
+        if logBuffer ~= "" then
+            discordrelay.CreateMessage(logChannel, "```"..logBuffer.."```",function(h,b,c)
+                if discordrelay.util.badcode[c] then
+                    abort = abort + 1
+                    logBuffer = "" -- clear buffer to get rid of potentally massive logs
+                    discordrelay.log(2,"DiscordRelayAddLog failed",discordrelay.util.badcode[c],"retrying",abort)
+                    return
+                else
+                    abort = 0
+                end
+                end)
+            logBuffer = ""
+        end
+    end)
+
+    hook.Add("EngineSpew", "DiscordRelaySpew", function(spewType, msg, group, level)
+        msg = string.gsub(msg,'"','\"')
+        msg = string.gsub(msg,"%d+%.%d+%.%d+%.%d+","XXX.XXX.XXX.XXX")
+        for i=1,#blacklist do
+            if string.match(msg, blacklist[i]) then
+                return
+            end
+        end
+
+        logBuffer = logBuffer..msg
+    end)
+end
+
+function scriptlog.Remove()
+    hook.Remove("EngineSpew", "DiscordRelaySpew")
+    timer.Destroy("DiscordRelayAddLog")
+    if discordrelay.modules.scriptlog then
+        discordrelay.modules.scriptlog = nil
+    end
+end
+
+return scriptlog
--- lua/discordrelay/modules/server/runlua.lua
+++ lua/discordrelay/modules/server/runlua.lua
@@ -4,6 +4,8 @@ local prefixes = discordrelay.prefixes
 local easylua = requirex('easylua')
 local luadev = requirex('luadev')
 
+if not easylua or not luadev then discordrelay.log(2,"easylua or luadev not found, runlua disabled.") return false end
+
 local function getType(cmds, msg)
     if not cmds or not msg then return end
     for k,v in pairs(prefixes) do
@@ -18,7 +20,6 @@ end
 
 function runlua.Init()
     local webhookid_scriptlog = "285359393124384770"
-
     local webhooktoken_scriptlog = file.Read( "webhook_token_scriptlog.txt", "DATA" )
 
     if not webhooktoken_scriptlog then
