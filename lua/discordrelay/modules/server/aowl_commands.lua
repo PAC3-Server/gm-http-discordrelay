32cbe48d48d2535f0a21b54aa3cf3e1c17e8bb51
techbot <techbot121@gmail.com>
2017-10-22 20:53:05 +0200

    show time and fix aowl commands

--- lua/discordrelay/modules/server/aowl_commands.lua
+++ lua/discordrelay/modules/server/aowl_commands.lua
@@ -5,7 +5,7 @@ function aowl_commands.Init()
     hook.Add("AowlCommand", "DiscordrelayAowlMsg", function(command, alias, ply, arg_line, args)
         local tracked = { "restart", "reboot", "rank", "ban", "unban", "kick" }
 
-        if not tracked[command] then return end
+        if not tracked[alias] then return end
 
         if ply then
             discordrelay.util.GetAvatar(ply:SteamID(), function(ret)
@@ -25,7 +25,7 @@ function aowl_commands.Init()
                             ["fields"] = {
                                 [1] = {
                                     ["name"] = "Command:",
-                                    ["value"] = command .. tostring(unpack(args)),
+                                    ["value"] = alias .. tostring(unpack(args)),
                                     ["inline"] = false
                                 }
                             },
@@ -49,7 +49,7 @@ function aowl_commands.Init()
                         ["fields"] = {
                             [1] = {
                                 ["name"] = "Command:",
-                                ["value"] = command .. tostring(unpack(args)),
+                                ["value"] = alias .. tostring(unpack(args)),
                                 ["inline"] = false
                             }
                         },
@@ -60,13 +60,15 @@ function aowl_commands.Init()
         end
     end)
 
-    hook.Add("AowlTargetCommand", "DiscordrelayBanMsg", function(ply, what, target, reason)
+    hook.Add("AowlTargetCommand", "DiscordrelayBanMsg", function(ply, what, target, extra)
         if what ~= "ban" then return end
         if discordrelay and discordrelay.enabled then
             local steamid = ply:SteamID()
             local targetid = type(target) == "string" and target or target:SteamID()
             local commid = util.SteamIDTo64(steamid)
             local url = "http://steamcommunity.com/profiles/" .. util.SteamIDTo64(targetid)
+            local time = extra["time"]
+            local reason = extra["reason"]
 
             discordrelay.util.GetAvatar(steamid, function(ret)
                 discordrelay.ExecuteWebhook(discordrelay.webhookid, discordrelay.webhooktoken, {
@@ -87,6 +89,11 @@ function aowl_commands.Init()
                                     ["name"] = "Reason:",
                                     ["value"] = reason,
                                     ["inline"] = false
+                                },
+                                [2] = {
+                                    ["name"] = "Time:",
+                                    ["value"] = string.NiceTime(time),
+                                    ["inline"] = false
                                 }
                             },
                             ["color"] = 0xb30000
@@ -97,13 +104,14 @@ function aowl_commands.Init()
         end
     end)
 
-    hook.Add("AowlTargetCommand", "DiscordrelayUnbanMsg", function(ply, what, target, reason)
+    hook.Add("AowlTargetCommand", "DiscordrelayUnbanMsg", function(ply, what, target, extra)
         if what ~= "unban" then return end
             if discordrelay and discordrelay.enabled then
             local steamid = ply:SteamID()
             local commid = util.SteamIDTo64(steamid)
             local targetid = type(target) == "string" and target or target:SteamID()
             local url = "http://steamcommunity.com/profiles/" .. util.SteamIDTo64(targetid)
+            local reason = extra["reason"]
 
             discordrelay.util.GetAvatar(steamid, function(ret)
                 discordrelay.ExecuteWebhook(discordrelay.webhookid, discordrelay.webhooktoken, {
