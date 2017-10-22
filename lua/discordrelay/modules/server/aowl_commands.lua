42cb6f1552464a7716e4f2ded8d21517dbcf696b
techbot <techbot121@gmail.com>
2017-10-22 21:26:18 +0200

    this should work now

--- lua/discordrelay/modules/server/aowl_commands.lua
+++ lua/discordrelay/modules/server/aowl_commands.lua
@@ -2,12 +2,18 @@ local aowl_commands = {}
 local discordrelay = discordrelay
 function aowl_commands.Init()
 
-    hook.Add("AowlCommand", "DiscordrelayAowlMsg", function(command, alias, ply, arg_line, args)
-        local tracked = { "restart", "reboot", "rank", "ban", "unban", "kick" }
-
+    hook.Add("AowlCommand", "DiscordrelayAowlMsg", function(_, alias, ply, arg_line, args)
+        local tracked = {
+            ["restart"] = true,
+            ["reboot"] = true,
+            ["rank"] = true,
+            ["ban"] = true,
+            ["unban"] = true,
+            ["kick"] = true
+        }
         if not tracked[alias] then return end
 
-        if ply then
+        if IsValid(ply) then
             discordrelay.util.GetAvatar(ply:SteamID(), function(ret)
                 discordrelay.ExecuteWebhook(discordrelay.webhookid, discordrelay.webhooktoken, {
                     ["username"] = discordrelay.username,
@@ -61,84 +67,74 @@ function aowl_commands.Init()
     end)
 
     hook.Add("AowlTargetCommand", "DiscordrelayBanMsg", function(ply, what, target, extra)
-        if what ~= "ban" then return end
+        if what ~= "ban" or what ~= "unban" then return end
         if discordrelay and discordrelay.enabled then
             local steamid = ply:SteamID()
             local targetid = type(target) == "string" and target or target:SteamID()
             local commid = util.SteamIDTo64(steamid)
             local url = "http://steamcommunity.com/profiles/" .. util.SteamIDTo64(targetid)
-            local time = extra["time"]
             local reason = extra["reason"]
-
-            discordrelay.util.GetAvatar(steamid, function(ret)
-                discordrelay.ExecuteWebhook(discordrelay.webhookid, discordrelay.webhooktoken, {
-                    ["username"] = discordrelay.username,
-                    ["avatar_url"] = discordrelay.avatar,
-                    ["embeds"] = {
-                        [1] = {
-                            ["title"] = "",
-                            ["description"] = "**BANNED:** " .. url,
-                            ["author"] = {
-                                ["name"] = (string.gsub(ply:Nick(),"<.->","")),
-                                ["icon_url"] = ret,
-                                ["url"] = "http://steamcommunity.com/profiles/" .. commid
-                            },
-                            ["type"] = "rich",
-                            ["fields"] = {
-                                [1] = {
-                                    ["name"] = "Reason:",
-                                    ["value"] = reason,
-                                    ["inline"] = false
+            if what == "ban" then
+                local time = extra["time"]
+                discordrelay.util.GetAvatar(steamid, function(ret)
+                    discordrelay.ExecuteWebhook(discordrelay.webhookid, discordrelay.webhooktoken, {
+                        ["username"] = discordrelay.username,
+                        ["avatar_url"] = discordrelay.avatar,
+                        ["embeds"] = {
+                            [1] = {
+                                ["title"] = "",
+                                ["description"] = "**BANNED:** " .. url,
+                                ["author"] = {
+                                    ["name"] = (string.gsub(ply:Nick(),"<.->","")),
+                                    ["icon_url"] = ret,
+                                    ["url"] = "http://steamcommunity.com/profiles/" .. commid
                                 },
-                                [2] = {
-                                    ["name"] = "Time:",
-                                    ["value"] = string.NiceTime(time),
-                                    ["inline"] = false
-                                }
-                            },
-                            ["color"] = 0xb30000
+                                ["type"] = "rich",
+                                ["fields"] = {
+                                    [1] = {
+                                        ["name"] = "Reason:",
+                                        ["value"] = reason,
+                                        ["inline"] = false
+                                    },
+                                    [2] = {
+                                        ["name"] = "Time:",
+                                        ["value"] = string.NiceTime(time),
+                                        ["inline"] = false
+                                    }
+                                },
+                                ["color"] = 0xb30000
+                            }
                         }
-                    }
-                })
-            end)
-        end
-    end)
-
-    hook.Add("AowlTargetCommand", "DiscordrelayUnbanMsg", function(ply, what, target, extra)
-        if what ~= "unban" then return end
-            if discordrelay and discordrelay.enabled then
-            local steamid = ply:SteamID()
-            local commid = util.SteamIDTo64(steamid)
-            local targetid = type(target) == "string" and target or target:SteamID()
-            local url = "http://steamcommunity.com/profiles/" .. util.SteamIDTo64(targetid)
-            local reason = extra["reason"]
-
-            discordrelay.util.GetAvatar(steamid, function(ret)
-                discordrelay.ExecuteWebhook(discordrelay.webhookid, discordrelay.webhooktoken, {
-                    ["username"] = discordrelay.username,
-                    ["avatar_url"] = discordrelay.avatar,
-                    ["embeds"] = {
-                        [1] = {
-                            ["title"] = "",
-                            ["description"] = "**UNBANNED:** " .. url,
-                            ["author"] = {
-                                ["name"] = (string.gsub(ply:Nick(),"<.->","")),
-                                ["icon_url"] = ret,
-                                ["url"] = "http://steamcommunity.com/profiles/" .. commid
-                            },
-                            ["type"] = "rich",
-                            ["fields"] = {
-                                [1] = {
-                                    ["name"] = "Reason:",
-                                    ["value"] = reason,
-                                    ["inline"] = false
-                                }
-                            },
-                            ["color"] = 0x00b300
+                    })
+                end)
+            else -- it can only be unban
+                discordrelay.util.GetAvatar(steamid, function(ret)
+                    discordrelay.ExecuteWebhook(discordrelay.webhookid, discordrelay.webhooktoken, {
+                        ["username"] = discordrelay.username,
+                        ["avatar_url"] = discordrelay.avatar,
+                        ["embeds"] = {
+                            [1] = {
+                                ["title"] = "",
+                                ["description"] = "**UNBANNED:** " .. url,
+                                ["author"] = {
+                                    ["name"] = (string.gsub(ply:Nick(),"<.->","")),
+                                    ["icon_url"] = ret,
+                                    ["url"] = "http://steamcommunity.com/profiles/" .. commid
+                                },
+                                ["type"] = "rich",
+                                ["fields"] = {
+                                    [1] = {
+                                        ["name"] = "Reason:",
+                                        ["value"] = reason,
+                                        ["inline"] = false
+                                    }
+                                },
+                                ["color"] = 0x00b300
+                            }
                         }
-                    }
-                })
-            end)
+                    })
+                end)
+            end
         end
     end)
 end
