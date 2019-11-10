8dde1e200c34dfeed5e9e46b63f0b2235f3c5497
Techbot121 <techbot121@gmail.com>
2019-11-10 18:05:57 +0100

    Update ingame_msg.lua
    apply fix from metaluvit repo

--- lua/discordrelay/modules/server/ingame_msg.lua
+++ lua/discordrelay/modules/server/ingame_msg.lua
@@ -1,6 +1,26 @@
 local ingame_msg = {}
 local discordrelay = discordrelay
 
+local function cleanMassPings(str)
+	local ok, n1, n2, n3
+
+	-- Strip bad combinations until they no longer exist or until we have iterated too many times
+	for i = 1, 32 do
+		str, n1 = str:gsub("%\xE2%\x80%\xAE", "") -- escape RTL chars that discord removes: https://github.com/Eufranio/MagiBridge/blob/6a946b0b32347b107b57fa947410d772104003ff/src/main/java/com/magitechserver/magibridge/discord/DiscordMessageBuilder.java#L32
+		str, n2 = str:gsub("@+([Ee][Vv][Ee][Rr][Yy][Oo][Nn][Ee])", "%1")
+		str, n3 = str:gsub("@+([Hh][Ee][Rr][Ee])", "%1")
+
+		if n1 + n2 + n3 == 0 then
+			ok = true
+			break
+		end
+	end
+
+	if not ok then return (str:gsub("[^a-zA-Z0-9]", "")) end
+
+	return str
+end
+
 function ingame_msg.Init()
 
 	hook.Add("PlayerSay", "DiscordRelayChat", function(ply, text, teamChat)
@@ -20,10 +40,7 @@ function ingame_msg.Init()
 				end
 			end
 				
-			text = text:gsub("%\x20[%\x2E%\x2D%\x2A%\x2B%\x2C]","")
-			text = text:gsub("(@+)everyone", "everyone")
-			text = text:gsub("(@+)here", "here")
-			
+			cleanMassPings(text)
 
 			text = text:gsub("<texture=(.-)>", function(url) return url end)
 
